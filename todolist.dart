137547dcea5ab6c3e8214c8eeda3a899078c8ff7
krishnadevasunil <119195073+krishnadevasunil@users.noreply.github.com>
2022-12-18 08:22:01 +0530

    Add files via upload

---
+++ todolist.dart
@@ -0,0 +1,81 @@
+
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+void main() => runApp(App());
+
+class TodoListState extends State<TodoList> {
+  final List<String> _todoList = <String>[];
+  final TextEditingController _textFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: ListView(children: _getItems()),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: const Icon(Icons.add)),
+    );
+  }
+
+  void _addTodoItem(String title) {
+    // Wrapping it inside a set state will notify
+    // the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+  // Generate list of item widgets
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+
+  // Generate a single item widget
+  Future<Future> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a task to your list'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: 'Enter task here'),
+            ),
+            actions: <Widget>[
+              dynamicFlatButton(
+                child: const Text('ADD'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              FlatButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  List<Widget> _getItems() {
+    final List<Widget> todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      todoWidgets.add(_buildTodoItem(title));
+    }
+    return todoWidgets;
+  }
+}
+
+dynamicFlatButton({required Text child, required Null Function()onPressed}) {
+}
+
+FlatButton({required Text child, required Null Function() onPressed}) async {
+}
